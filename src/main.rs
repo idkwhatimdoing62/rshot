0285e8c0ccba20932e7ff724022490f1bac0d4d1
qwq <qwq@homemail.com>
2026-07-06 18:10:05 +0800

    全局快捷键触发截图

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 use arboard::{Clipboard, ImageData};
+use global_hotkey::{
+    GlobalHotKeyEvent, GlobalHotKeyManager,
+    hotkey::{Code, HotKey, Modifiers},
+};
 use mouse_position::mouse_position::Mouse;
 use std::error::Error;
+use windows::Win32::UI::WindowsAndMessaging::{
+    DispatchMessageW,
+    GetMessageW,
+    MSG, // ← MSG 加进来
+};
 use xcap::Monitor;
-
 fn main() -> Result<(), Box<dyn Error>> {
+    let manager = GlobalHotKeyManager::new()?;
+    let hotkey = HotKey::new(Some(Modifiers::ALT), Code::KeyA);
+    manager.register(hotkey)?; // 订阅热键监听
+
+    let receiver = GlobalHotKeyEvent::receiver(); // 拿到信箱
+    let mut msg = MSG::default();
+    unsafe {
+        while GetMessageW(&mut msg, None, 0, 0).as_bool() { // 取出一条消息
+            DispatchMessageW(&msg); // 派发消息给信箱
+            if let Ok(_) = receiver.try_recv() { //取出消息
+                capture_to_clipboard()?;
+            }
+        }
+    }
+    Ok(())
+}
+
+fn capture_to_clipboard() -> Result<(), Box<dyn Error>> {
     let position = Mouse::get_mouse_position();
     let Mouse::Position { x, y } = position else {
         return Err("拿不到鼠标坐标".into()); // 不匹配就早退
